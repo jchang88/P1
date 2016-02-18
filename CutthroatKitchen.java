29ab25174fd0e80688e5bd923cb2019f2a4974a4
JS 600.226 Sp16 <cs22666@ugradx.cs.jhu.edu>
2016-02-17 20:05:25 -0500

    HELL YES got the right output, need to implement penalty counter

--- CutthroatKitchen.java
+++ CutthroatKitchen.java
@@ -25,15 +25,19 @@ public class CutthroatKitchen {
      * @param args the argument
      */
     public static void main(String[] args) throws IOException {
+
         //keeps track of the accumulation of penalty points
         int penalties = 0;
-        CList<CookingStation> game = new CList<CookingStation>();
 
+        // circular list of stations
+        CList<CookingStation> game = new CList<CookingStation>();
+        
+        // scan file input and read into game CList
         Scanner infile = null;
         boolean inerror = false;
 
         try {
-            System.out.println("0 " + args[0] + " should be input filename");
+            // args[0] should be input filename
             infile = new Scanner(new FileReader(args[0]));
         } catch (ArrayIndexOutOfBoundsException a) {
             System.err.println("must give input filename at command line");
@@ -46,7 +50,7 @@ public class CutthroatKitchen {
             System.err.println("exiting...");
             System.exit(1);
         }
-        
+
         Scanner inline;
         String line;
         String name, item;
@@ -63,22 +67,30 @@ public class CutthroatKitchen {
                 over = inline.nextInt();
                 CookingItem tempItem = new CookingItem(item, time, under, over);
                 tempStation.addItem(tempItem);
-                //name += " " + item + " " + time + " " + under + " " + over;
                 line = infile.nextLine();
             }
             game.append(tempStation);
-            //System.out.println(name);
         }
         
+        // tick until all stations are done
         while (game.length() != 0) {
-        //for (int x = 0; x < 50; x++) { 
-            // Save current position
-            int pos = game.currPos();
+            // Remove any empty stations
+            for (int i = 0; i < game.length(); i++) {
+                if (game.getValue().length() == 0) {
+                    game.remove();
+                }
+                game.cycle();
+            }
+
+            // Save current station position
+            int stationPos = game.currPos();
 
             // Making string to output
             String kitchen = "[ ";
             game.moveToStart();
             for (int i = 0; i < game.length(); i++) {
+                // save current item position
+                int itemPos = game.getValue().currPos();
                 kitchen += game.getValue().getNameS() + " [";
                 game.getValue().moveToStart();
                 for (int j = 0; j < game.getValue().length(); j++) {
@@ -88,6 +100,7 @@ public class CutthroatKitchen {
                     game.getValue().cycle();
                 }
                 kitchen += " ] ";
+                game.getValue().moveToPos(itemPos);
                 game.cycle();
             }
             kitchen += "]\n";
@@ -97,24 +110,21 @@ public class CutthroatKitchen {
             System.out.print(kitchen);
             
             
-            // tend current item at current station and cycle
-            game.moveToPos(pos);
-            System.out.print(game.currPos());
-            game.getValue().tend(2, 1000);
-            game.getValue().cycle();
-            game.cycle();
+            // tend current item at current station
+            game.moveToPos(stationPos);
+            CookingItem temp = game.getValue().tend(2, 1000);
 
-            if (game.getValue().length() == 0) {
-                game.remove();
+            // only cycle if tend doesn't remove an item, remove already "cycles" to the next item
+            if (temp == null) {
+                game.getValue().cycle();
             }
+            game.cycle();
 
             // tick every station
             for (int i = 0; i < game.length(); i++) {
                 game.getValue().tick();
                 game.cycle();
             }
-            
-
         }
     }
 }
